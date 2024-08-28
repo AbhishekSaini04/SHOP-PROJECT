6cba536428fed411ca5dcef0f3aea36fee6feed7
Astro <85232708+AbhishekSaini04@users.noreply.github.com>
2024-08-28 10:14:21 +0530

    New

--- routes/blog/addBlog.js
+++ routes/blog/addBlog.js
@@ -1,39 +1,34 @@
-const express=require("express");
-const router=express.Router();
-const multer=require("multer");
-const {blogModel}=require("../../models/blogModel");
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const { blogModel } = require("../../models/blogModel");
 // const {brandModel}=require("../../models/brandModel");
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './uploads/blogImage')
-    },
-    filename: function (req, file, cb) {
-
-      cb(null, `${Date.now()}-${file.originalname}` );
-    }
-  })
-  
-  const upload = multer({ storage:storage})
-
-router.post("/addblog",upload.single("image"),async(req,res)=>{
-  let {name,brand,problem}=req.body;
-  if(!(name && brand && req.file &&problem)){return res.end("All Fields are Required")};
-
-
-
-  blogModel.create({
-    name:name,
-    problem:problem,
-    brand:brand.toUpperCase(),
-    image:`/blogImage/${req.file.filename}`,
-  })
-
-
-   return res.redirect("/dashboard/addblog");
-
+  destination: function (req, file, cb) {
+    cb(null, "./uploads/blogImage");
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
 });
 
+const upload = multer({ storage: storage });
 
+router.post("/addblog", upload.single("image"), async (req, res) => {
+  let { name, brand, problem } = req.body;
+  if (!(name && brand && req.file && problem)) {
+    return res.end("All Fields are Required");
+  }
 
-module.exports=router;
+  blogModel.create({
+    name: name,
+    problem: problem,
+    brand: brand.toUpperCase(),
+    image: `/blogImage/${req.file.filename}`,
+  });
+
+  return res.redirect("/dashboard/addblog");
+});
+
+module.exports = router;
--- routes/staticRoute.js
+++ routes/staticRoute.js
@@ -1,98 +1,103 @@
-const express=require("express");
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
 // const {shortURL}=require("../models/shortURLModel");
-const {restrictTo}=require("../middlewares/auth");
-const {productModel}=require("../models/productsModel");
+const { restrictTo } = require("../middlewares/auth");
+const { productModel } = require("../models/productsModel");
 // const {brandModel}=require("../models/brandModel");
-const {blogModel}=require("../models/blogModel");
-const {duplicateElementRemover}=require("../controller/duplicateElementRemover");
+const { blogModel } = require("../models/blogModel");
+const {
+  duplicateElementRemover,
+} = require("../controller/duplicateElementRemover");
 
+router.get("/", async (req, res) => {
+  const user = req?.user;
+  // console.log(user);
+  const blogs = await blogModel.find({});
 
-router.get("/",async(req,res)=>{
-
-const user=req?.user;
-// console.log(user);
-const blogs=await blogModel.find({});
-
-    return res.render("home",{user:user,blogs:blogs});
+  return res.render("home", { user: user, blogs: blogs });
 });
 
-router.get("/signup",(req,res)=>{
-    return res.render("signup");
+router.get("/signup", (req, res) => {
+  return res.render("signup");
+});
 
-})
-
-router.get("/login",(req,res)=>{
-    return res.render("login");
-
-})
+router.get("/login", (req, res) => {
+  return res.render("login");
+});
 // DASHBOARD
 
-router.get("/dashboard",(req,res)=>{
-    return res.render("dashboard/dashboard.ejs",{user:req.user});
-
+router.get("/dashboard", restrictTo(["ADMIN"]), (req, res) => {
+  return res.render("dashboard/dashboard.ejs", { user: req.user });
 });
-
 
 //all uploaded products
-router.get("/dashboard/uploadedProducts",restrictTo(["ADMIN"]),async(req,res)=>{
-   const allProducts=await productModel.find({});
-//    console.log(allProducts);
-    return res.render("dashboard/uploadedproducts",{
-        allProducts:allProducts,
+router.get(
+  "/dashboard/uploadedProducts",
+  restrictTo(["ADMIN"]),
+  async (req, res) => {
+    const allProducts = await productModel.find({});
+    //    console.log(allProducts);
+    return res.render("dashboard/uploadedproducts", {
+      allProducts: allProducts,
     });
-});
+  }
+);
 
 //add new product
-router.get("/dashboard/addproduct",restrictTo(["ADMIN"]),(req,res)=>{
-    return res.render("dashboard/addproduct",{condition:true});
+router.get("/dashboard/addproduct", restrictTo(["ADMIN"]), (req, res) => {
+  return res.render("dashboard/addproduct", { condition: true });
 });
 
 // update exsisting products
-router.get("/dashboard/updateProduct/:id",restrictTo(["ADMIN"]),(req,res)=>{
-   
-    return res.render("dashboard/addproduct",{condition:false,id:req.params.id});
-});
-
+router.get(
+  "/dashboard/updateProduct/:id",
+  restrictTo(["ADMIN"]),
+  (req, res) => {
+    return res.render("dashboard/addproduct", {
+      condition: false,
+      id: req.params.id,
+    });
+  }
+);
 
 // blog route
-router.get("/blog/:blogId",async(req,res)=>{
-    const blogId=req.params?.blogId;
-    let blog;
-    try{
-        blog=await blogModel.findOne({_id:blogId});
-    }catch(e){
-       return res.redirect("/");
-    }
-    if(!blogId){
-        return res.redirect("/");
-        
-    }
-return res.render("blog",{blog:blog, user:req.user})
+router.get("/blog/:blogId", async (req, res) => {
+  const blogId = req.params?.blogId;
+  let blog;
+  try {
+    blog = await blogModel.findOne({ _id: blogId });
+  } catch (e) {
+    return res.redirect("/");
+  }
+  if (!blogId) {
+    return res.redirect("/");
+  }
+  return res.render("blog", { blog: blog, user: req.user });
 });
-
 
 // add Blog
-router.get("/dashboard/addblog",restrictTo(["ADMIN"]),(req,res)=>{
-   
-    return res.render("dashboard/addBlog",{condition:true});
+router.get("/dashboard/addblog", restrictTo(["ADMIN"]), (req, res) => {
+  return res.render("dashboard/addBlog", { condition: true });
 });
 // update exsisting blog
-router.get("/dashboard/updateblog/:id",restrictTo(["ADMIN"]),(req,res)=>{
-   
-    return res.render("dashboard/addblog",{condition:false,id:req.params.id});
+router.get("/dashboard/updateblog/:id", restrictTo(["ADMIN"]), (req, res) => {
+  return res.render("dashboard/addblog", {
+    condition: false,
+    id: req.params.id,
+  });
 });
 
 //all uploaded blog
-router.get("/dashboard/uploadedBlogs",restrictTo(["ADMIN"]),async(req,res)=>{
-    const allBlogs=await blogModel.find({});
- //    console.log(allProducts);
-     return res.render("dashboard/uploadedBlogs",{
-         allBlogs:allBlogs,
-     });
- });
- 
+router.get(
+  "/dashboard/uploadedBlogs",
+  restrictTo(["ADMIN"]),
+  async (req, res) => {
+    const allBlogs = await blogModel.find({});
+    //    console.log(allProducts);
+    return res.render("dashboard/uploadedBlogs", {
+      allBlogs: allBlogs,
+    });
+  }
+);
 
-
-
-module.exports=router;
+module.exports = router;
